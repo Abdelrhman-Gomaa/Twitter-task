ce7fe13d2fd8c42540dc61ebb6f7723a5270b44e
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-10 16:21:13 +0200

    add provider

--- src/follower/follower.module.ts
+++ src/follower/follower.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FollowerService } from './follower.service';
 import { FollowerResolver } from './follower.resolver';
+import { DatabaseModule } from 'src/database/database.module';
+import { FollowerProviders } from './follower.provider';
 
 @Module({
-  providers: [FollowerResolver, FollowerService]
+  imports:[DatabaseModule],
+  providers: [
+    FollowerResolver, 
+    FollowerService,
+    ...FollowerProviders
+  ]
 })
 export class FollowerModule {}
