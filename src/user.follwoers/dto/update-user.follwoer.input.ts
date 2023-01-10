c4822fca1499dfacc91528a857eafc6b43b41c9a
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-10 14:15:15 +0200

    delete old follower resource

--- src/user.follwoers/dto/update-user.follwoer.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserFollwoerInput } from './create-user.follwoer.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateUserFollwoerInput extends PartialType(CreateUserFollwoerInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/user.follwoers/user.follwoers.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserFollwoersService } from './user.follwoers.service';
-import { UserFollwoersResolver } from './user.follwoers.resolver';
-
-@Module({
-  providers: [UserFollwoersResolver, UserFollwoersService]
-})
-export class UserFollwoersModule {}
