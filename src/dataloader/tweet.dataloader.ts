3ff3d97360e444bd33611e323f39b51ad39d8e59
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-15 01:40:03 +0200

    feat(): tweetloader method

---
+++ src/dataloader/tweet.dataloader.ts
@@ -0,0 +1,19 @@
+import * as DataLoader from 'DataLoader'
+import { Tweet } from 'src/tweet/entities/tweet.entity';
+import { TweetService } from 'src/tweet/tweet.service';
+
+type BatchTweet = (ids: number[]) => Promise<Tweet[]>;
+
+const batchTweets: BatchTweet = async ids => {
+
+    const tweets = await Tweet.findAll({where: {userId:ids}});
+  
+    const tweetMap: { [key: number]: Tweet } = {};
+    tweets.forEach(u => {
+        tweetMap[u.id] = u;
+    });
+  
+    return ids.map(id => tweetMap[id]);
+  };
+  
+  export const TweetLoader = () => new DataLoader<number, Tweet>(batchTweets);
