b5f557f24279a3522b0eedfd2dd68f51a21fc306
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-01-14 16:08:42 +0200

    feat(): import modules

--- src/react/react.module.ts
+++ src/react/react.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ReactService } from './react.service';
 import { ReactResolver } from './react.resolver';
+import { DatabaseModule } from 'src/database/database.module';
+import { ReactProviders } from './react.provider';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { UserProviders } from 'src/user/users.provider';
+import { TweetService } from 'src/tweet/tweet.service';
+import { TweetProviders } from 'src/tweet/tweet.provider';
 
 @Module({
-  providers: [ReactResolver, ReactService]
+  imports:[DatabaseModule],
+  providers: [
+    ReactResolver, 
+    ReactService,
+    ...ReactProviders,
+    UserService,
+    ...UserProviders,
+    TweetService,
+    ...TweetProviders,
+    JwtService
+  ],
 })
 export class ReactModule {}
